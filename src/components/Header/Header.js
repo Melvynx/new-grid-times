27cbc9283011cf2f6e0277f1dbcacb0eff5fcf57
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2021-11-28 16:20:34 +0100

    add header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -29,16 +29,66 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopAction>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopAction>
         <Logo />
+        <SubscribeContainer>
+          <Button>Subscribe</Button>
+          <SubscribeText>Already a subscriber?</SubscribeText>
+        </SubscribeContainer>
       </MainHeader>
     </header>
   );
 };
 
+const SubscribeContainer = styled.div`
+  display: none;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+  }
+`;
+
+const SubscribeText = styled.a`
+  font-weight: ${WEIGHTS['400']};
+  font-style: italic;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const DesktopAction = styled.div`
+  display: none;
+  gap: 24px;
+
+  /*
+  FIX: Remove the inline spacing that comes with
+  react-feather icons.
+*/
+  svg {
+    display: block;
+  }
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+  }
+`;
+
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +115,10 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    justify-content: space-between;
+  }
 `;
 
 export default Header;
